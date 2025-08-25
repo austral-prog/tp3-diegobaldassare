fb0f4f47f9fe9e4c19b04822648aecb3882c75a2
Diego Baldassare <diegobaldassare@gmail.com>
2025-08-25 17:22:44 -0300

    Done excercises

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,9 @@
 def slice_advanced():
     # Código a implementar utilizando input.
 
+    # Ingresar un texto usando input e imprimir en pantalla el resultado de obtener los valores saltando de a 2 caracteres, pero sólo a partir del quinto carácter ( incluído )
+    texto = input("Ingresa un texto: ")
+    print(texto[4::2].lower())
+
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
--- slice_simple.py
+++ slice_simple.py
@@ -4,5 +4,15 @@ def slice_simple():
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
 
 
+    # Obtener los siguientes datos del texto de ejemplo e imprimirlos en pantalla:
+    #
+    # Primeras 3 letras del texto ( usando índices positivos )
+    # Las 3 letras en medio del texto ( usando índices positivos )
+    # De la primera a la cuarta letra ( incluída ) y de la antepenúltima hasta la última ( incluída )
+    # Es necesario que todos los caracteres se impriman en minúscula
+    print(texto[:3].lower())
+    print(texto[2:5].lower())
+    print(texto[:4].lower() + texto[-3:].lower())
+
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
